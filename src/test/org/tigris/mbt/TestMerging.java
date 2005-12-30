58e05488cf79d38fadac9e53cfa4b8653dcba444
KristianKarl <KristianKarl@124b672a-51f8-0310-8ca3-83fb5af575ed>
2005-12-30 09:29:29 +0000

    Miss-spelled folder names

--- src/test/org/tigris/mbt/TestMerging.java
+++ src/test/org/tigris/mbt/TestMerging.java
@@ -20,7 +20,7 @@ public class TestMerging extends TestCase
     // Test merging of 2 simple graphs
     public void test01()
     {
-    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/Test01" );
+    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/test01" );
     	assertTrue( mbt.getGraph().getEdges().size() == 9 );
     	assertTrue( mbt.getGraph().getVertices().size() == 6 );
     	mbt.writeGraph( "graphml/merged/test01.graphml" );
@@ -29,7 +29,7 @@ public class TestMerging extends TestCase
     // Test merging of 2 simple graphs, with  nodes containing key word NO_MERGE
     public void test02()
     {
-    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/Test02" );
+    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/test02" );
     	assertTrue( mbt.getGraph().getEdges().size() == 11 );
     	assertTrue( mbt.getGraph().getVertices().size() == 7 );
     	mbt.writeGraph( "graphml/merged/test02.graphml" );
@@ -38,7 +38,7 @@ public class TestMerging extends TestCase
     // Test merging of 22 complex graphs
     public void test03()
     {
-    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/Test03" );
+    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/test03" );
     	assertTrue( mbt.getGraph().getEdges().size() == 415 );
     	assertTrue( mbt.getGraph().getVertices().size() == 180 );
     	mbt.writeGraph( "graphml/merged/test03.graphml" );
