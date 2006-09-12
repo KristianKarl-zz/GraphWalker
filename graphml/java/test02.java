1af63b35f75074d496453f2f0396583fd36872fd
KristianKarl <KristianKarl@124b672a-51f8-0310-8ca3-83fb5af575ed>
2006-09-12 19:54:37 +0000

    Added some files for test purposes

--- graphml/java/test02.java
+++ graphml/java/test02.java
@@ -1,27 +1,9 @@
 /**
- * This method implements the verification of the vertex: B
- */
-public void B()
-{
-	_logger.info( "Vertex: B" );
-	throw new RuntimeException( "Not implemented. This line can be removed." );
-}
-
-/**
  * This method implements the verification of the vertex: C
  */
 public void C()
 {
 	_logger.info( "Vertex: C" );
-	throw new RuntimeException( "Not implemented. This line can be removed." );
-}
-
-/**
- * This method implements the verification of the vertex: E
- */
-public void E()
-{
-	_logger.info( "Vertex: E" );
 	throw new RuntimeException( "Not implemented. This line can be removed." );
 }
 
@@ -44,6 +26,24 @@ public void A()
 }
 
 /**
+ * This method implements the verification of the vertex: B
+ */
+public void B()
+{
+	_logger.info( "Vertex: B" );
+	throw new RuntimeException( "Not implemented. This line can be removed." );
+}
+
+/**
+ * This method implements the verification of the vertex: E
+ */
+public void E()
+{
+	_logger.info( "Vertex: E" );
+	throw new RuntimeException( "Not implemented. This line can be removed." );
+}
+
+/**
  * This method implemets the edge: doB
  */
 public void doB()
@@ -62,15 +62,6 @@ public void doCSpecial()
 }
 
 /**
- * This method implemets the edge: doC
- */
-public void doC()
-{
-	_logger.info( "Edge: doC" );
-	throw new RuntimeException( "Not implemented" );
-}
-
-/**
  * This method implemets the edge: doA
  */
 public void doA()
@@ -80,20 +71,29 @@ public void doA()
 }
 
 /**
- * This method implemets the edge: doE
- */
-public void doE()
-{
-	_logger.info( "Edge: doE" );
-	throw new RuntimeException( "Not implemented" );
-}
-
-/**
  * This method implemets the edge: doD
  */
 public void doD()
 {
 	_logger.info( "Edge: doD" );
+	throw new RuntimeException( "Not implemented" );
+}
+
+/**
+ * This method implemets the edge: doC
+ */
+public void doC()
+{
+	_logger.info( "Edge: doC" );
+	throw new RuntimeException( "Not implemented" );
+}
+
+/**
+ * This method implemets the edge: doE
+ */
+public void doE()
+{
+	_logger.info( "Edge: doE" );
 	throw new RuntimeException( "Not implemented" );
 }
 
