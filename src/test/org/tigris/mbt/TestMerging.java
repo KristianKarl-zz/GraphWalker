b96318d0c3cd26715433fcbbaa8aa8f13630e258
KristianKarl <KristianKarl@124b672a-51f8-0310-8ca3-83fb5af575ed>
2006-09-12 19:52:44 +0000

    Added more JUnit tests

--- src/test/org/tigris/mbt/TestMerging.java
+++ src/test/org/tigris/mbt/TestMerging.java
@@ -17,6 +17,7 @@ public class TestMerging extends TestCase
         junit.textui.TestRunner.main(testCaseName);
     }
 
+
     // Test merging of 2 simple graphs
     public void test01()
     {
@@ -29,6 +30,7 @@ public class TestMerging extends TestCase
     	}
     	catch ( RuntimeException e)
     	{
+    		System.out.println(e.getMessage());
     		fail( e.getMessage() );
     	}
     }
@@ -45,6 +47,7 @@ public class TestMerging extends TestCase
     	}
     	catch ( RuntimeException e)
     	{
+    		System.out.println(e.getMessage());
     		fail( e.getMessage() );
     	}
     }
@@ -61,6 +64,7 @@ public class TestMerging extends TestCase
     	}
     	catch ( RuntimeException e)
     	{
+    		System.out.println(e.getMessage());
     		fail( e.getMessage() );
     	}
     }
@@ -74,7 +78,7 @@ public class TestMerging extends TestCase
     		mbt.generateTests();
     	} catch (Exception e) {
     		String msg = e.getMessage();
-    		System.out.println( msg );
+    		System.out.println(e.getMessage());
     		assertTrue( msg.matches( ".*There is no way to reach.*|.*Found a cul-de-sac, I have to stop now.*" ) );
 		}
     }
@@ -89,6 +93,7 @@ public class TestMerging extends TestCase
 		}
 		catch ( Exception e)
 		{
+    		System.out.println(e.getMessage());
 			fail( e.getMessage() );
 		}
     }
@@ -108,6 +113,7 @@ public class TestMerging extends TestCase
 		}
 		catch ( RuntimeException e)
 		{
+    		System.out.println(e.getMessage());
 			fail( e.getMessage() );
 		}
     }
@@ -127,7 +133,120 @@ public class TestMerging extends TestCase
 		}
 		catch ( RuntimeException e)
 		{
+    		System.out.println(e.getMessage());
 			fail( e.getMessage() );
 		}
+    }    
+    
+    // Verify that mbt reports and exits when an edge without name is found 
+    public void test08()
+    {
+    	try
+    	{
+	    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/test08" );
+	    	fail( "Missing error message" );
+    	}
+    	catch ( RuntimeException e)
+    	{
+    		String msg = e.getMessage();
+    		System.out.println(e.getMessage());
+    		assertTrue( msg.matches( "Label for an edge comming from a non-Start vertex,  'null \\(C -> A\\) [0-9]+\\([0-9]+ -> [0-9]+\\)', must be defined in file: \\\".*graphml.test08.graph1.graphml\\\"" ) );
+    	}
+    }
+
+    // Verify that mbt checks that subgraphs are unique.
+    public void test09()
+    {
+    	try
+    	{
+	    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/test09" );
+	    	fail( "Missing error message" );
+    	}
+    	catch ( RuntimeException e)
+    	{
+    		String msg = e.getMessage();
+    		System.out.println(e.getMessage());
+    		assertTrue( msg.matches( "Found 2 subgraphs using the same name: 'B', they are defined in files: '.*\\\\graphml.test09.C.graphml', and :'.*graphml.test09.B\\.graphml'" ) );
+    	}
+    }
+
+    // Verify that mbt reports and exits when an node without name is found 
+    public void test10()
+    {
+    	try
+    	{
+	    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/test10" );
+	    	fail( "Missing error message" );
+    	}
+    	catch ( RuntimeException e)
+    	{
+    		String msg = e.getMessage();
+    		System.out.println(e.getMessage());
+    		assertTrue( msg.matches( "Vertex is missing its label in file: \".*test10.B\\.graphml\"" ) );
+    	}
+    }
+
+    // Verify that mbt reports and exits when a recursive subgraph situation emerges 
+    public void test11()
+    {
+    	try
+    	{
+	    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/test11" );
+	    	fail( "Missing error message" );
+    	}
+    	catch ( RuntimeException e)
+    	{
+    		String msg = e.getMessage();
+    		System.out.println(e.getMessage());
+    		assertTrue( msg.matches( "Found a subgraph containing a duplicate vertex with name: C, in file: '.*test11.C\\.graphml'" ) );
+    	}
+    }
+    
+    // Verify that mbt reports and exits when an edge without name is found 
+    public void test12()
+    {
+    	try
+    	{
+	    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/test12" );
+	    	fail( "Missing error message" );
+    	}
+    	catch ( RuntimeException e)
+    	{
+    		String msg = e.getMessage();
+    		System.out.println(e.getMessage());
+    		assertTrue( msg.matches( "Label for an edge comming from a non-Start vertex,  'null \\(E -> G\\) [0-9]+\\([0-9]+ -> [0-9]+\\)', must be defined in file: \\\".*graphml.test12.C\\.graphml\\\"" ) );
+    	}
+    }
+    
+    // Verify that mbt reports and exits when an edge containing a whitespace (tab) is found 
+    public void test13()
+    {
+    	try
+    	{
+	    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/test13" );
+	    	fail( "Missing error message" );
+    	}
+    	catch ( RuntimeException e)
+    	{
+    		String msg = e.getMessage();
+    		System.out.println(e.getMessage());
+    		assertTrue( msg.matches( "Label for an edge comming from a non-Start vertex,  'null \\(E -> G\\) [0-9]+\\([0-9]+ -> [0-9]+\\)', must be defined in file: \\\".*graphml.test13.C\\.graphml\\\"" ) );
+    	}
+    }
+    
+    // Verify that mbt reports and exits when a vertex containing a whitespace (tab) is found 
+    public void test14()
+    {
+    	try
+    	{
+	    	ModelBasedTesting mbt = new ModelBasedTesting( "graphml/test14" );
+	    	fail( "Missing error message" );
+    	}
+    	catch ( RuntimeException e)
+    	{
+    		String msg = e.getMessage();
+    		System.out.println(e.getMessage());
+    		assertTrue( msg.matches( "Vertex has a label 'Containing a whitespace', containing whitespaces in file: \\\".*graphml.test14.C\\.graphml\\\"" ) );
+    	}
     }
 }
