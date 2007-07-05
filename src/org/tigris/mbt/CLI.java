2318ad7b5b9f833ff75b711f1afecc425cf14419
KristianKarl <KristianKarl@124b672a-51f8-0310-8ca3-83fb5af575ed>
2007-07-05 09:32:54 +0000

    New merging rules/verification

--- src/org/tigris/mbt/CLI.java
+++ src/org/tigris/mbt/CLI.java
@@ -343,7 +343,7 @@ public class CLI
 			}
 			else if ( args[ 0 ].equals( "-v" ) || args[ 0 ].equals( "--version" ) )
 			{
-				System.out.println( "org.tigris.mbt version 1.0 (r146)\n" );
+				System.out.println( "org.tigris.mbt version 1.1 (r156)\n" );
 				System.out.println( "org.tigris.mbt is open source software licensed under GPL" );
 				System.out.println( "The software (and it's source) can be downloaded at http://mbt.tigris.org/\n" );
 				System.out.println( "This package contains following software packages:" );
