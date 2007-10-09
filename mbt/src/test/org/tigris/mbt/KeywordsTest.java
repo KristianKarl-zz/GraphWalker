34656b9fac314ac1ce9b60305e42fa7e9d4e3559
KristianKarl <KristianKarl@124b672a-51f8-0310-8ca3-83fb5af575ed>
2007-10-09 12:57:00 +0000

    Added test, and fixed a bug.

---
+++ mbt/src/test/org/tigris/mbt/KeywordsTest.java
@@ -0,0 +1,28 @@
+package test.org.tigris.mbt;
+
+import org.tigris.mbt.Keywords;
+
+import junit.framework.TestCase;
+
+public class KeywordsTest extends TestCase {
+
+	public void testKeywords() 
+	{
+		assertEquals( true, Keywords.isKeyWord( "BACKTRACK" ) );
+		assertEquals( true, Keywords.isKeyWord( "BLOCKED" ) );
+		assertEquals( true, Keywords.isKeyWord( "MERGE" ) );
+		assertEquals( true, Keywords.isKeyWord( "NO_MERGE" ) );		
+	}
+
+	public void testNonKeywords() 
+	{
+		assertEquals( false, Keywords.isKeyWord( "BACKTRACKING" ) );
+		assertEquals( false, Keywords.isKeyWord( "BLOCK" ) );
+		assertEquals( false, Keywords.isKeyWord( "MERGED" ) );
+		assertEquals( false, Keywords.isKeyWord( "NO_MERGED" ) );		
+		assertEquals( false, Keywords.isKeyWord( "backtrack" ) );
+		assertEquals( false, Keywords.isKeyWord( "blocked" ) );
+		assertEquals( false, Keywords.isKeyWord( "merge" ) );
+		assertEquals( false, Keywords.isKeyWord( "no_merge" ) );		
+	}
+}
