b7119fa75e10a33d4438e63b0272231a092a48c4
tejle <tejle@124b672a-51f8-0310-8ca3-83fb5af575ed>
2007-10-11 12:53:38 +0000

    Added FSM and EFSM as basic structures

---
+++ src/org/tigris/mbt/ExtendedFiniteStateMachine.java
@@ -0,0 +1,44 @@
+package org.tigris.mbt;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import edu.uci.ics.jung.graph.impl.DirectedSparseEdge;
+import edu.uci.ics.jung.graph.impl.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.impl.DirectedSparseVertex;
+
+public class ExtendedFiniteStateMachine extends FiniteStateMachine {
+
+	private Hashtable dataStore = new Hashtable();
+	public String getCurrentStateName()
+	{
+		return super.getCurrentStateName() + "/" + getCurrentData();
+	}
+
+	public ExtendedFiniteStateMachine(DirectedSparseGraph newModel)
+	{
+		super(newModel);
+		model.getEdgeConstraints().add(new AccessableEdgeFilter(dataStore));
+	}
+	
+	public String getCurrentData() {
+		String retur = "";
+		Enumeration keys = dataStore.keys();
+		while(keys.hasMoreElements())
+		{
+			String key = (String) keys.nextElement();
+			retur += key + "=" + (String) dataStore.get(key);
+			if(keys.hasMoreElements()) retur += ":";
+		}
+		return retur;
+	}
+	
+	public void walkEdge(DirectedSparseEdge edge)
+	{
+		if(currentState.isSource(edge))
+		{
+			//TODO: add parsing of possible actions 
+			currentState = (DirectedSparseVertex) edge.getDest();
+		}
+	}
+}
