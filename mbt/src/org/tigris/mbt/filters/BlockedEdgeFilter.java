23a04f313beae51ec9613e041ad278af52760010
tejle <tejle@124b672a-51f8-0310-8ca3-83fb5af575ed>
2007-10-11 14:05:35 +0000

    Moved to clean up folder (and it makes more sense this way)

---
+++ mbt/src/org/tigris/mbt/filters/BlockedEdgeFilter.java
@@ -0,0 +1,16 @@
+package org.tigris.mbt.filters;
+
+import edu.uci.ics.jung.graph.Edge;
+import edu.uci.ics.jung.graph.filters.GeneralEdgeAcceptFilter;
+
+public class BlockedEdgeFilter extends GeneralEdgeAcceptFilter {
+
+	public boolean acceptEdge(Edge edge) {
+		return !edge.containsUserDatumKey("BLOCKED");
+	}
+
+	public String getName() {
+		return "BlockedEdgeFilter";
+	}
+
+}
