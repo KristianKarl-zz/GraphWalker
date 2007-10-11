bf06f0e2727ec1a05bd4fdb20daa551bab1c4874
tejle <tejle@124b672a-51f8-0310-8ca3-83fb5af575ed>
2007-10-11 14:05:35 +0000

    Moved to clean up folder (and it makes more sense this way)

---
+++ src/org/tigris/mbt/filters/BlockedVertexFilter.java
@@ -0,0 +1,16 @@
+package org.tigris.mbt.filters;
+
+import edu.uci.ics.jung.graph.Vertex;
+import edu.uci.ics.jung.graph.filters.GeneralVertexAcceptFilter;
+
+public class BlockedVertexFilter extends GeneralVertexAcceptFilter {
+
+	public boolean acceptVertex(Vertex vertex) {
+		return !vertex.containsUserDatumKey("BLOCKED");
+	}
+
+	public String getName() {
+		return "BlockedVertexFilter";
+	}
+
+}
