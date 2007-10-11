731f0532f231f8f6c52b00612dac88c8f198a25c
tejle <tejle@124b672a-51f8-0310-8ca3-83fb5af575ed>
2007-10-11 13:58:01 +0000

    Updated to fit new class structure

--- mbt/src/test/org/tigris/mbt/AccessableEdgeFilterTest.java
+++ mbt/src/test/org/tigris/mbt/AccessableEdgeFilterTest.java
@@ -1,7 +1,10 @@
 package test.org.tigris.mbt;
 
+import java.util.Hashtable;
+
 import org.tigris.mbt.AccessableEdgeFilter;
-import org.tigris.mbt.AbstractModel;
+import org.tigris.mbt.Keywords;
+
 import edu.uci.ics.jung.graph.impl.DirectedSparseEdge;
 import edu.uci.ics.jung.graph.impl.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.impl.DirectedSparseVertex;
@@ -11,9 +14,12 @@ import junit.framework.TestCase;
 public class AccessableEdgeFilterTest extends TestCase {
 	private AccessableEdgeFilter f;
 	private DirectedSparseEdge e;
+	private Hashtable dataStore;
 	
 	protected void setUp() throws Exception {
 		super.setUp();
+		dataStore = new Hashtable();
+		
 		DirectedSparseVertex v1 = new DirectedSparseVertex();
 		DirectedSparseVertex v2 = new DirectedSparseVertex();
 		DirectedSparseGraph g = new DirectedSparseGraph();
@@ -23,116 +29,116 @@ public class AccessableEdgeFilterTest extends TestCase {
 		e = new DirectedSparseEdge(v1, v2);
 		g.addEdge(e);
 
-		e.addUserDatum(AbstractModel.LABEL_KEY, "", UserData.SHARED);
+		e.addUserDatum(Keywords.LABEL_KEY, "", UserData.SHARED);
 
-		f = new AccessableEdgeFilter();
+		f = new AccessableEdgeFilter(dataStore);
 	}
 
 	public void testAcceptEdgeEdge1()
 	{
-		e.setUserDatum(AbstractModel.LABEL_KEY, "", UserData.SHARED);
+		e.setUserDatum(Keywords.LABEL_KEY, "", UserData.SHARED);
 		assertEquals(true, f.acceptEdge(e));
 	}
 
 	public void testAcceptEdgeEdge2() 
 	{
-		e.setUserDatum(AbstractModel.LABEL_KEY, "test", UserData.SHARED);
+		e.setUserDatum(Keywords.LABEL_KEY, "test", UserData.SHARED);
 		assertEquals(true, f.acceptEdge(e));
 	}
 	
 	public void testAcceptEdgeEdge3() 
 	{
-		e.setUserDatum(AbstractModel.LABEL_KEY, "test [true]", UserData.SHARED);
+		e.setUserDatum(Keywords.LABEL_KEY, "test [true]", UserData.SHARED);
 		assertEquals(true, f.acceptEdge(e));
 	}
 	
 	public void testAcceptEdgeEdge4() 
 	{
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[true]", UserData.SHARED);
+		e.setUserDatum(Keywords.LABEL_KEY, "[true]", UserData.SHARED);
 		assertEquals(true, f.acceptEdge(e));
 	}
 
 	public void testAcceptEdgeEdge5() 
 	{
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[true] / test", UserData.SHARED);
+		e.setUserDatum(Keywords.LABEL_KEY, "[true] / test", UserData.SHARED);
 		assertEquals(true, f.acceptEdge(e));
 	}
 
 	public void testAcceptEdgeEdge6() 
 	{
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[false]", UserData.SHARED);
+		e.setUserDatum(Keywords.LABEL_KEY, "[false]", UserData.SHARED);
 		assertEquals(false, f.acceptEdge(e));
 	}
 	
 	public void testAcceptEdgeEdge7() 
 	{
-		AbstractModel.setDataStore("X", "false");
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[X]", UserData.SHARED);
+		dataStore.put("X", "false");
+		e.setUserDatum(Keywords.LABEL_KEY, "[X]", UserData.SHARED);
 		assertEquals(false, f.acceptEdge(e));
 	}
 
 	public void testAcceptEdgeEdge8() 
 	{
-		AbstractModel.setDataStore("X", "true");
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[X]", UserData.SHARED);
+		dataStore.put("X", "true");
+		e.setUserDatum(Keywords.LABEL_KEY, "[X]", UserData.SHARED);
 		assertEquals(true, f.acceptEdge(e));
 	}
 
 	public void testAcceptEdgeEdge9() 
 	{
-		AbstractModel.setDataStore("X", "true");
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[X=true]", UserData.SHARED);
+		dataStore.put("X", "true");
+		e.setUserDatum(Keywords.LABEL_KEY, "[X=true]", UserData.SHARED);
 		assertEquals(true, f.acceptEdge(e));
 	}
 
 	public void testAcceptEdgeEdge10() 
 	{
-		AbstractModel.setDataStore("X", "true");
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[X=false]", UserData.SHARED);
+		dataStore.put("X", "true");
+		e.setUserDatum(Keywords.LABEL_KEY, "[X=false]", UserData.SHARED);
 		assertEquals(false, f.acceptEdge(e));
 	}
 
 	public void testAcceptEdgeEdge11() 
 	{
-		AbstractModel.setDataStore("X", "true");
-		AbstractModel.setDataStore("Y", "true");
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[X=Y]", UserData.SHARED);
+		dataStore.put("X", "true");
+		dataStore.put("Y", "true");
+		e.setUserDatum(Keywords.LABEL_KEY, "[X=Y]", UserData.SHARED);
 		assertEquals(true, f.acceptEdge(e));
 	}
 
 	public void testAcceptEdgeEdge12() 
 	{
-		AbstractModel.setDataStore("X", "true");
-		AbstractModel.setDataStore("Y", "false");
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[X=Y]", UserData.SHARED);
+		dataStore.put("X", "true");
+		dataStore.put("Y", "false");
+		e.setUserDatum(Keywords.LABEL_KEY, "[X=Y]", UserData.SHARED);
 		assertEquals(false, f.acceptEdge(e));
 	}
 
 	public void testAcceptEdgeEdge13() 
 	{
-		AbstractModel.setDataStore("X", "5");
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[X=5]", UserData.SHARED);
+		dataStore.put("X", "5");
+		e.setUserDatum(Keywords.LABEL_KEY, "[X=5]", UserData.SHARED);
 		assertEquals(true, f.acceptEdge(e));
 	}
 
 	public void testAcceptEdgeEdge14() 
 	{
-		AbstractModel.setDataStore("X", "6");
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[X=5]", UserData.SHARED);
+		dataStore.put("X", "6");
+		e.setUserDatum(Keywords.LABEL_KEY, "[X=5]", UserData.SHARED);
 		assertEquals(false, f.acceptEdge(e));
 	}
 
 	public void testAcceptEdgeEdge15() 
 	{
-		AbstractModel.setDataStore("X", "6");
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[X>5]", UserData.SHARED);
+		dataStore.put("X", "6");
+		e.setUserDatum(Keywords.LABEL_KEY, "[X>5]", UserData.SHARED);
 		assertEquals(true, f.acceptEdge(e));
 	}
 
 	public void testAcceptEdgeEdge16() 
 	{
-		AbstractModel.setDataStore("X", "4");
-		e.setUserDatum(AbstractModel.LABEL_KEY, "[X<5]", UserData.SHARED);
+		dataStore.put("X", "4");
+		e.setUserDatum(Keywords.LABEL_KEY, "[X<5]", UserData.SHARED);
 		assertEquals(true, f.acceptEdge(e));
 	}
 }
