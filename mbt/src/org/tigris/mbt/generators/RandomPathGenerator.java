490246bf8f9c7f977653660fdb4b9b1079fffcef
tejle <tejle@124b672a-51f8-0310-8ca3-83fb5af575ed>
2007-10-21 17:51:01 +0000

    REFACTOR: just a cleanup, no fixes.

--- mbt/src/org/tigris/mbt/generators/RandomPathGenerator.java
+++ mbt/src/org/tigris/mbt/generators/RandomPathGenerator.java
@@ -23,37 +23,43 @@ public class RandomPathGenerator extends PathGenerator {
 	public String[] getNext() {
 		Util.AbortIf(!hasNext(), "No more lines available");
 		Set availableEdges = machine.getCurrentOutEdges();
-		DirectedSparseEdge edge = null;
 		Util.AbortIf(availableEdges.size() == 0, "Found a dead end: '" + machine.getCurrentStateName() + "'"); 
-		if( machine.isWeighted() )
-		{
-			Vector zeroes = new Vector();
-			float sum = 0;
-			float limit = random.nextFloat();
-
-			for ( Iterator i = availableEdges.iterator(); i.hasNext();)
-			{
-				DirectedSparseEdge e = (DirectedSparseEdge)i.next();
-				Float weight = (Float) e.getUserDatum( Keywords.WEIGHT_KEY );
-				if(weight == null)
-				{
-					zeroes.add(e);
-				} else {
-					sum += weight.floatValue();
-					if(sum >= limit && edge == null) edge = e;
-				}
-			}
-			Util.AbortIf( sum > 1 ,"The weight of out edges excceds 1 for " + machine.getCurrentStateName() );	
-			if( edge == null )
-			{
-				edge = (DirectedSparseEdge) zeroes.get(random.nextInt(zeroes.size()));
-			}
-		}else{
-			edge = (DirectedSparseEdge) availableEdges.toArray()[random.nextInt(availableEdges.size())];
-		}
+		DirectedSparseEdge edge = (machine.isWeighted() ? getWeightedEdge(availableEdges) : getRandomEdge(availableEdges));
 		machine.walkEdge(edge);
 		String[] retur = {machine.getEdgeName(edge), machine.getCurrentStateName()};
 		return retur;
+	}
+	
+	private DirectedSparseEdge getWeightedEdge(Set availableEdges)
+	{
+		DirectedSparseEdge edge = null;
+		Vector zeroes = new Vector();
+		float sum = 0;
+		float limit = random.nextFloat();
+
+		for ( Iterator i = availableEdges.iterator(); i.hasNext();)
+		{
+			DirectedSparseEdge e = (DirectedSparseEdge)i.next();
+			Float weight = (Float) e.getUserDatum( Keywords.WEIGHT_KEY );
+			if(weight == null)
+			{
+				zeroes.add(e);
+			} else {
+				sum += weight.floatValue();
+				if(sum >= limit && edge == null) edge = e;
+			}
+		}
+		Util.AbortIf( sum > 1 ,"The weight of out edges excceds 1 for " + machine.getCurrentStateName() );
+		if( edge == null )
+		{
+			edge = (DirectedSparseEdge) zeroes.get(random.nextInt(zeroes.size()));
+		}
+		return edge;
+	}
+	
+	private DirectedSparseEdge getRandomEdge(Set availableEdges)
+	{
+		return (DirectedSparseEdge) availableEdges.toArray()[random.nextInt(availableEdges.size())];
 	}
 
 }
