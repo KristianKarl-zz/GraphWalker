d74138bacdb201b85989fdd89a7503214f75a9b6
Tejle <Tejle@124b672a-51f8-0310-8ca3-83fb5af575ed>
2007-11-04 02:02:13 +0000

    Added documentation from Util Class

--- src/org/tigris/mbt/generators/CodeGenerator.java
+++ src/org/tigris/mbt/generators/CodeGenerator.java
@@ -2,6 +2,25 @@ package org.tigris.mbt.generators;
 
 import org.tigris.mbt.FiniteStateMachine;
 
+/**
+ * Will generate code using a template. The code generated will contain all lables/names
+ * defined by the vertices and edges. This enables the user to write templates for a 
+ * multitude of scripting or programming languages.<br><br>
+ * There is 2 variables in the template that will be replaced as follows:<br>
+ * <strong>{LABEL}</strong> Will be replace by the actual name of the edge or vertex.<br>
+ * <strong>{EDGE_VERTEX}</strong> Will be replace by the word 'Edge' or 'Vertex'.<br><br>
+ * <strong>Below is an example of a template.</strong>
+ * <pre>
+ * /**
+ * * This method implements the {EDGE_VERTEX} '{LABEL}'
+ * * /
+ * public void {LABEL}()
+ * {
+ *    log.info( "{EDGE_VERTEX}: {LABEL}" );
+ *    throw new RuntimeException( "Not implemented" );
+ * }
+ * </pre>
+ */
 public class CodeGenerator extends ListGenerator {
 
 	String template;
