2e0f7a3ac4b51240c953666ccbee41834fb0563d
tejle <tejle@124b672a-51f8-0310-8ca3-83fb5af575ed>
2008-01-08 08:59:48 +0000

    REFACTOR: moved Exceptions to own sub package

--- src/org/tigris/mbt/exceptions/GoBackToPreviousVertexException.java
+++ src/org/tigris/mbt/exceptions/GoBackToPreviousVertexException.java
@@ -15,7 +15,7 @@
 //along with this program; if not, write to the Free Software
 //Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-package org.tigris.mbt;
+package org.tigris.mbt.exceptions;
 
 /**
  * This exception can be thrown in order to backtrack the current point of execution
