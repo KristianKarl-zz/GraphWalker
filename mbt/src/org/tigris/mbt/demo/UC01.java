9d80ebcdea4bd782c85b9583668ec97d4c2571bf
KristianKarl <KristianKarl@124b672a-51f8-0310-8ca3-83fb5af575ed>
2008-01-24 22:16:48 +0000

    CHANGE: Changed the demo to a simple use case: adding books on www.amazon.com

--- mbt/src/org/tigris/mbt/demo/UC01.java
+++ mbt/src/org/tigris/mbt/demo/UC01.java
@@ -1,5 +1,8 @@
 package org.tigris.mbt.demo;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import junit.framework.TestCase;
 
 import org.apache.log4j.Logger;
@@ -7,20 +10,23 @@ import org.openqa.selenium.server.SeleniumServer;
 import org.tigris.mbt.Keywords;
 import org.tigris.mbt.ModelBasedTesting;
 import org.tigris.mbt.Util;
+import org.tigris.mbt.exceptions.InvalidDataException;
 
 import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.Selenium;
 
 public class UC01 extends TestCase
 {
-    private Selenium selenium;
+	ModelBasedTesting mbt = new ModelBasedTesting();
+    private Selenium browser;
 	private Logger log;
 
 	public void testUC01() 
 	{
 		log = Util.setupLogger( UC01.class );
-
-		ModelBasedTesting mbt = new ModelBasedTesting();
+		
+		// Tell mbt which model to use
+		mbt.enableExtended( true );
 		
 		// Tell mbt which model to use
 		mbt.readGraph( "demo/Model/UC01.graphml" );
@@ -36,21 +42,12 @@ public class UC01 extends TestCase
 	}
 	
 	/**
-	 * This method implements the Edge 'e_DirectLink'
-	 */
-	public void e_DirectLink()
-	{
-	  log.info( "Edge: e_DirectLink" );
-	  selenium.open("http://mbt.tigris.org/source/browse/mbt/branches/1.15/mbt/.classpath?view=markup");
-	}
-
-	/**
 	 * This method implements the Edge 'e_EnterBaseURL'
 	 */
 	public void e_EnterBaseURL()
 	{
 		log.info( "Edge: e_EnterBaseURL" );
-		selenium.open("http://mbt.tigris.org");
+		browser.open("http://www.amazon.com");
 	}
 
 	/**
@@ -59,7 +56,7 @@ public class UC01 extends TestCase
 	public void e_StartBrowser()
 	{
 		log.info( "Edge: e_StartBrowser" );
-		selenium.start();
+		browser.start();
 	}
 
 	/**
@@ -68,7 +65,7 @@ public class UC01 extends TestCase
 	public void e_StopBrowser()
 	{
 	  log.info( "Edge: e_StopBrowser" );
-	  selenium.stop();
+	  browser.stop();
 	}
 
 	/**
@@ -77,7 +74,7 @@ public class UC01 extends TestCase
 	public void e_SubversionLink()
 	{
 	  log.info( "Edge: e_SubversionLink" );
-	  selenium.click( "link=Subversion" );
+	  browser.click( "link=Subversion" );
 	}
 
 	/**
@@ -88,15 +85,15 @@ public class UC01 extends TestCase
 		log.info( "Edge: e_init" );
 		SeleniumServer selServer = new SeleniumServer();
 		selServer.start();
-		String url = "http://mbt.tigris.org";
+		String url = "http://www.amazon.com";
 		
 		// For a linux machine with firefox installed in '/usr/lib/mozilla-firefox/'
 		// where the firefox executable is not in the path.
 		// See also: http://release.openqa.org/selenium-remote-control/0.9.2/doc/java/com/thoughtworks/selenium/DefaultSelenium.html#DefaultSelenium(java.lang.String,%20int,%20java.lang.String,%20java.lang.String)
-		//selenium = new DefaultSelenium("localhost", SeleniumServer.getDefaultPort(), "*firefox /usr/lib/mozilla-firefox/firefox-bin", url);
+		browser = new DefaultSelenium("localhost", SeleniumServer.getDefaultPort(), "*firefox /usr/lib/mozilla-firefox/firefox-bin", url);
 
 		// Should work for any machine where the firefox executable is in the path.
-		selenium = new DefaultSelenium("localhost", SeleniumServer.getDefaultPort(), "*firefox", url);
+		//browser = new DefaultSelenium("localhost", SeleniumServer.getDefaultPort(), "*firefox", url);
 	}
 
 	/**
@@ -105,8 +102,8 @@ public class UC01 extends TestCase
 	public void v_BaseSubversionURL()
 	{
 	  log.info( "Vertex: v_BaseSubversionURL" );
-	  selenium.waitForPageToLoad( "10000" );
-	  assertEquals( "mbt: Subversion", selenium.getTitle() );
+	  browser.waitForPageToLoad( "10000" );
+	  assertEquals( "mbt: Subversion", browser.getTitle() );
 	}
 
 	/**
@@ -115,8 +112,8 @@ public class UC01 extends TestCase
 	public void v_BaseURL()
 	{
 	  log.info( "Vertex: v_BaseURL" );
-	  selenium.waitForPageToLoad( "10000" );
-	  assertEquals( "mbt.tigris.org", selenium.getTitle() );
+	  browser.waitForPageToLoad( "10000" );
+	  assertEquals( "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more", browser.getTitle() );
 	}
 
 	/**
@@ -128,7 +125,7 @@ public class UC01 extends TestCase
 		String title = null;
 		try
 		{
-		  title = selenium.getTitle();
+		  title = browser.getTitle();
 		}
 		catch ( Exception e)
 		{
@@ -146,7 +143,7 @@ public class UC01 extends TestCase
 		String title = null;
 		try
 		{
-		  title = selenium.getTitle();
+		  title = browser.getTitle();
 		}
 		catch ( Exception e)
 		{
@@ -154,4 +151,110 @@ public class UC01 extends TestCase
 		}
 		fail( "We should not have reach this code. Found an open browser: " + title );
 	}
+	
+	/**
+	 * This method implements the Edge 'e_AddBookToCart'
+	 */
+	public void e_AddBookToCart()
+	{
+	  log.info( "Edge: e_AddBookToCart" );
+	  browser.click("submit.add-to-cart");
+	}
+
+	/**
+	 * This method implements the Edge 'e_ClickBook'
+	 */
+	public void e_ClickBook()
+	{
+	  log.info( "Edge: e_ClickBook" );
+	  browser.click("link=regexp:Practical Model-Based Testing: A Tools Approach");
+	}
+
+	/**
+	 * This method implements the Edge 'e_SearchBook'
+	 */
+	public void e_SearchBook()
+	{
+	  log.info( "Edge: e_SearchBook" );
+	  browser.type("twotabsearchtextbox", "model-based testing");
+	  browser.click("navGoButtonPanel");
+	}
+
+	/**
+	 * This method implements the Edge 'e_ShoppingCart'
+	 */
+	public void e_ShoppingCart()
+	{
+	  log.info( "Edge: e_ShoppingCart" );
+	  browser.click("//img[@alt='Cart']");
+	}
+
+	/**
+	 * This method implements the Vertex 'v_BookInformation'
+	 */
+	public void v_BookInformation()
+	{
+	  log.info( "Vertex: v_BookInformation" );
+	  browser.waitForPageToLoad("30000");
+	  String body = browser.getBodyText();
+	  Pattern pattern = Pattern.compile( "Upgrade this book " );
+	  Matcher matcher = pattern.matcher( body );
+	  assertTrue( matcher.find() );
+	  pattern = Pattern.compile( "Availability: " );
+	  matcher = pattern.matcher( body );
+	  assertTrue( matcher.find() );
+	}
+
+	/**
+	 * This method implements the Vertex 'v_OtherBoughtBooks'
+	 */
+	public void v_OtherBoughtBooks()
+	{
+	  log.info( "Vertex: v_OtherBoughtBooks" );
+	  browser.waitForPageToLoad("30000");
+	  String body = browser.getBodyText();
+	  Pattern pattern = Pattern.compile( "Customers who bought Practical Model-Based Testing: A Tools Approach also bought:" );
+	  Matcher matcher = pattern.matcher( body );
+	  assertTrue( matcher.find() );	  
+	}
+
+	/**
+	 * This method implements the Vertex 'v_SearchResult'
+	 */
+	public void v_SearchResult()
+	{
+	  log.info( "Vertex: v_SearchResult" );
+	  browser.waitForPageToLoad("30000");
+	  String body = browser.getBodyText();
+	  Pattern pattern = Pattern.compile( "Practical Model-Based Testing: A Tools Approach" );
+	  Matcher matcher = pattern.matcher( body );
+	  assertTrue( matcher.find() );
+	}
+
+	/**
+	 * This method implements the Vertex 'v_ShoppingCart'
+	 */
+	public void v_ShoppingCart()
+	{
+	  log.info( "Vertex: v_ShoppingCart" );
+	  browser.waitForPageToLoad("30000");
+	  String expectedValue = "";
+	  try {
+		  expectedValue = mbt.getDataValue( "num_of_books" );
+	  } catch (InvalidDataException e) {
+		fail( e.getMessage() );
+	  }
+	  if ( expectedValue.equals("0") )
+	  {
+		  String body = browser.getBodyText();
+		  Pattern pattern = Pattern.compile( "Your Shopping Cart is empty" );
+		  Matcher matcher = pattern.matcher( body );
+		  assertTrue( matcher.find() );
+		  return;
+	  }	  
+	  String actualValue = browser.getValue("quantity.1");
+	  assertTrue( actualValue.equals( expectedValue ) );
+	}
 }
+
+
