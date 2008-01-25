7d44e1c4f9cd9150f8d33d39b1d8715a0a2db08f
tejle <tejle@124b672a-51f8-0310-8ca3-83fb5af575ed>
2008-01-25 13:35:25 +0000

    ADD: Added testcases for combiantional conditions

---
+++ mbt/src/test/org/tigris/mbt/conditions/CombinationalConditionTest.java
@@ -0,0 +1,49 @@
+package test.org.tigris.mbt.conditions;
+
+import org.tigris.mbt.conditions.AlwaysCondition;
+import org.tigris.mbt.conditions.CombinationalCondition;
+import org.tigris.mbt.conditions.NeverCondition;
+
+import junit.framework.TestCase;
+
+public class CombinationalConditionTest extends TestCase {
+
+	public void testConstructor()
+	{
+		new CombinationalCondition();
+	}
+
+	public void testFulfillment()
+	{
+		CombinationalCondition condition = new CombinationalCondition();
+		condition.add(new AlwaysCondition());
+		assertEquals((double)1/1, condition.getFulfillment(), 0.01);
+		condition.add(new AlwaysCondition());
+		assertEquals((double)2/2, condition.getFulfillment(), 0.01);
+		condition.add(new NeverCondition());
+		assertEquals((double)2/3, condition.getFulfillment(), 0.01);
+		condition.add(new AlwaysCondition());
+		assertEquals((double)3/4, condition.getFulfillment(), 0.01);
+		condition.add(new NeverCondition());
+		assertEquals((double)3/5, condition.getFulfillment(), 0.01);
+		condition.add(new NeverCondition());
+		assertEquals((double)3/6, condition.getFulfillment(), 0.01);
+	}
+
+	public void testIsFulfilled()
+	{
+		CombinationalCondition condition = new CombinationalCondition();
+		condition.add(new AlwaysCondition());
+		assertEquals(true, condition.isFulfilled());
+		condition.add(new AlwaysCondition());
+		assertEquals(true, condition.isFulfilled());
+		condition.add(new NeverCondition());
+		assertEquals(false, condition.isFulfilled());
+		condition.add(new AlwaysCondition());
+		assertEquals(false, condition.isFulfilled());
+		condition.add(new NeverCondition());
+		assertEquals(false, condition.isFulfilled());
+		condition.add(new NeverCondition());
+		assertEquals(false, condition.isFulfilled());
+	}
+}
