70db62a1d560985c6d1cf14629d3a645d61eca08
tejle <tejle@124b672a-51f8-0310-8ca3-83fb5af575ed>
2008-01-25 13:27:20 +0000

    UPDATE: fixed to match same pattern

--- src/org/tigris/mbt/conditions/AlwaysCondition.java
+++ src/org/tigris/mbt/conditions/AlwaysCondition.java
@@ -3,6 +3,6 @@ package org.tigris.mbt.conditions;
 public class AlwaysCondition extends StopCondition 
 {
 	public AlwaysCondition() {}
-	public double getFulfillment() { return 1; }
 	public boolean isFulfilled() { return true; }
+	public double getFulfillment() { return 1; }
 }
