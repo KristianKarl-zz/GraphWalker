96e13cf066d8f8c8773c2e3fa7c36e483f1ae470
KristianKarl <KristianKarl@124b672a-51f8-0310-8ca3-83fb5af575ed>
2008-02-03 13:34:39 +0000

    ADD: JUnit tests for Keywords

--- src/test/org/tigris/mbt/KeywordsTest.java
+++ src/test/org/tigris/mbt/KeywordsTest.java
@@ -25,15 +25,40 @@ public class KeywordsTest extends TestCase {
 		assertEquals( false, Keywords.isKeyWord( "merge" ) );
 		assertEquals( false, Keywords.isKeyWord( "no_merge" ) );		
 		assertEquals( false, Keywords.isKeyWord( "REQTAG" ) );		
-	}
-
-	public void testEmpty() 
-	{
 		assertEquals( false, Keywords.isKeyWord( "" ) );		
+		assertEquals( false, Keywords.isKeyWord( null ) );		
 	}
 
-	public void testNull() 
+	public void testGetStopCondition() 
 	{
-		assertEquals( false, Keywords.isKeyWord( null ) );		
+		assertTrue( Keywords.getStopCondition( "REACHED_EDGE" ) != -1 );		
+		assertTrue( Keywords.getStopCondition( "REACHED_STATE" ) != -1 );		
+		assertTrue( Keywords.getStopCondition( "EDGE_COVERAGE" ) != -1 );		
+		assertTrue( Keywords.getStopCondition( "STATE_COVERAGE" ) != -1 );		
+		assertTrue( Keywords.getStopCondition( "TEST_LENGTH" ) != -1 );		
+		assertTrue( Keywords.getStopCondition( "TEST_DURATION" ) != -1 );		
+		assertTrue( Keywords.getStopCondition( "REQUIREMENT_COVERAGE" ) != -1 );		
+		assertTrue( Keywords.getStopCondition( "REACHED_REQUIREMENT" ) != -1 );	
+
+		assertTrue( Keywords.getStopCondition( "REACHEDREQUIREMENT" ) == -1 );	
+		assertTrue( Keywords.getStopCondition( "" ) == -1 );	
+		assertTrue( Keywords.getStopCondition( null ) == -1 );	
+		
+		assertTrue( Keywords.getStopConditions().size() > 0 );
+	}
+
+	public void testGetGenerator() 
+	{
+		assertTrue( Keywords.getGenerator( "RANDOM" ) != -1 );
+		assertTrue( Keywords.getGenerator( "SHORTEST" ) != -1 );
+		assertTrue( Keywords.getGenerator( "LIST" ) != -1 );
+		assertTrue( Keywords.getGenerator( "STUB" ) != -1 );
+		assertTrue( Keywords.getGenerator( "REQUIREMENTS" ) != -1 );
+
+		assertTrue( Keywords.getGenerator( "RASNDOM" ) == -1 );
+		assertTrue( Keywords.getGenerator( "" ) == -1 );
+		assertTrue( Keywords.getGenerator( null ) == -1 );
+		
+		assertTrue( Keywords.getGenerators().size() > 0 );
 	}
 }
