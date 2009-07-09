96b3bfa6c6aebdda1b52d5b76d0ffb250dcadb24
KristianKarl <KristianKarl@124b672a-51f8-0310-8ca3-83fb5af575ed>
2009-07-09 09:01:25 +0000

    FIX Start, pause, stop and reload for SOAP execution

--- mbt/src/org/tigris/mbt/GUI/Status.java
+++ mbt/src/org/tigris/mbt/GUI/Status.java
@@ -12,32 +12,44 @@ public class Status {
 	public static final int paused = 4;
 	public static final int stopped = 8;
 	public static final int executingJavaTest = 16;
+	public static final int executingSoapTest = 32;
 	
 	
+	public boolean isExecutingSoapTest() {
+		log.debug( "State: " + state );
+		return (state & executingSoapTest) == executingSoapTest;
+	}
 	public boolean isExecutingJavaTest() {
+		log.debug( "State: " + state );
 		return (state & executingJavaTest) == executingJavaTest;
 	}
 	public boolean isStopped() {
+		log.debug( "State: " + state );
 		return (state & stopped) == stopped;
 	}
 	public boolean isNext() {
+		log.debug( "State: " + state );
 		return (state & next) == next;
 	}
 	public boolean isRunning() {
+		log.debug( "State: " + state );
 		return (state & running) == running;
 	}
 	public boolean isPaused() {
+		log.debug( "State: " + state );
 		return (state & paused) == paused;
 	}
 	public void setState( int state ) {
 		log.debug( "Set the state with: " + state );
 		this.state |= state;
-		log.debug( "State: " + state );
+		log.debug( "State: " + this.state );
 	}
 	public void unsetState(int state) {
 		log.debug( "Unset the state with: " + state );
-		this.state ^= state;
-		log.debug( "State: " + state );
+		if ( (this.state & state) == state ) {
+			this.state ^= state;
+		}
+		log.debug( "State: " + this.state );
 	}
 	public void reset() {
 		log.debug( "Reset the state to stopped" );
