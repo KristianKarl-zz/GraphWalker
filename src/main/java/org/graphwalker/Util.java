435d6f795cfbb9521e23a71197b627ed3e3b775e
kristiankarl <kristiankarl@124b672a-51f8-0310-8ca3-83fb5af575ed>
2010-10-18 08:37:48 +0000

    Fixed warning

--- src/main/java/org/graphwalker/Util.java
+++ src/main/java/org/graphwalker/Util.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
