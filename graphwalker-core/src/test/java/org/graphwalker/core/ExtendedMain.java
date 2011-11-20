d5f97ee36afaf0d7f2048a018ce25fbead320f7e
Nils Olsson <nils.olsson@sylog.se>
2011-11-20 23:39:08 +0100

    Moved test class to the test source folder

---
+++ graphwalker-core/src/test/java/org/graphwalker/core/ExtendedMain.java
@@ -0,0 +1,161 @@
+/*
+ * #%L
+ * GraphWalker Core
+ * %%
+ * Copyright (C) 2011 GraphWalker
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+/**
+ *
+ */
+package org.graphwalker.core;
+
+/**
+ * <p>ExtendedMain class.</p>
+ */
+public class ExtendedMain {
+    /**
+     * This method implements the Edge 'e_Cancel'
+     */
+    public void e_Cancel() {
+        System.out.println("Edge: e_Cancel");
+    }
+
+    /**
+     * This method implements the Edge 'e_ChangeDatabase'
+     */
+    public void e_ChangeDatabase() {
+        System.out.println("Edge: e_ChangeDatabase");
+    }
+
+    /**
+     * This method implements the Edge 'e_CloseApp'
+     */
+    public void e_CloseApp() {
+        System.out.println("Edge: e_CloseApp");
+    }
+
+    /**
+     * This method implements the Edge 'e_CloseDB'
+     */
+    public void e_CloseDB() {
+        System.out.println("Edge: e_CloseDB");
+    }
+
+    /**
+     * This method implements the Edge 'e_CloseDialog'
+     */
+    public void e_CloseDialog() {
+        System.out.println("Edge: e_CloseDialog");
+    }
+
+    /**
+     * This method implements the Edge 'e_EnterCorrectKey'
+     */
+    public void e_EnterCorrectKey() {
+        System.out.println("Edge: e_EnterCorrectKey");
+    }
+
+    /**
+     * This method implements the Edge 'e_EnterInvalidKey'
+     */
+    public void e_EnterInvalidKey() {
+        System.out.println("Edge: e_EnterInvalidKey");
+    }
+
+    /**
+     * This method implements the Edge 'e_Initialize'
+     */
+    public void e_Initialize() {
+        System.out.println("Edge: e_Initialize");
+    }
+
+    /**
+     * This method implements the Edge 'e_No'
+     */
+    public void e_No() {
+        System.out.println("Edge: e_No");
+    }
+
+    /**
+     * This method implements the Edge 'e_Start'
+     */
+    public void e_Start() {
+        System.out.println("Edge: e_Start");
+    }
+
+    /**
+     * This method implements the Edge 'e_StartWithDatabase'
+     */
+    public void e_StartWithDatabase() {
+        System.out.println("Edge: e_StartWithDatabase");
+    }
+
+    /**
+     * This method implements the Edge 'e_Yes'
+     */
+    public void e_Yes() {
+        System.out.println("Edge: e_Yes");
+    }
+
+    /**
+     * This method implements the Vertex 'v_EnterMasterCompositeMasterKey'
+     */
+    public void v_EnterMasterCompositeMasterKey() {
+        System.out.println("Vertex: v_EnterMasterCompositeMasterKey");
+    }
+
+    /**
+     * This method implements the Vertex 'v_InvalidKey'
+     */
+    public void v_InvalidKey() {
+        System.out.println("Vertex: v_InvalidKey");
+    }
+
+    /**
+     * This method implements the Vertex 'v_KeePassNotRunning'
+     */
+    public void v_KeePassNotRunning() {
+        System.out.println("Vertex: v_KeePassNotRunning");
+    }
+
+    /**
+     * This method implements the Vertex 'v_MainWindowEmpty'
+     */
+    public void v_MainWindowEmpty() {
+        System.out.println("Vertex: v_MainWindowEmpty");
+    }
+
+    /**
+     * This method implements the Vertex 'v_MainWindow_DB_Loaded'
+     */
+    public void v_MainWindow_DB_Loaded() {
+        System.out.println("Vertex: v_MainWindow_DB_Loaded");
+    }
+
+    /**
+     * This method implements the Vertex 'v_SaveBeforeCloseLock'
+     */
+    public void v_SaveBeforeCloseLock() {
+        System.out.println("Vertex: v_SaveBeforeCloseLock");
+    }
+}
