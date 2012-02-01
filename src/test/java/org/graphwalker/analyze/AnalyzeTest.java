029066d9b207f1de183d2b1d2d586f05111076f3
Kristian Karl <kristian.hermann.karl@gmail.com>
2012-02-01 18:16:29 +0100

    ADD: Added Analyze function which anylzes a model for non-reachable vertices. Also, added possibility to retreive current requirement

---
+++ src/test/java/org/graphwalker/analyze/AnalyzeTest.java
@@ -0,0 +1,23 @@
+package org.graphwalker.analyze;
+
+import static org.junit.Assert.*;
+
+import org.graphwalker.ModelBasedTesting;
+import org.junit.Test;
+
+public class AnalyzeTest {
+
+  @Test
+  public void test() {
+    ModelBasedTesting mbt = new ModelBasedTesting();
+    mbt.readGraph("graphml/analyze/infinite-loop.graphml");
+    String str = Analyze.unreachableVertices(mbt);
+    assertTrue("We should have found vertices not reachable from one another", str.length() > 0);
+    
+    mbt = new ModelBasedTesting();
+    mbt.readGraph("graphml/analyze/non-infinite-loop.graphml");
+    str = Analyze.unreachableVertices(mbt);
+    assertTrue("We should not have found any vertices not reachable from one another", str.length() == 0);
+  }
+
+}
