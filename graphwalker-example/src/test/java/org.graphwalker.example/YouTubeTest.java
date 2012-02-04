0a73b4cf61e47a21be1f0242ba7d3dc10deea74d
Nils Olsson <nils.olsson@sylog.se>
2012-02-04 18:38:02 +0100

    A new example testing some navigation on the youtube site

---
+++ graphwalker-example/src/test/java/org.graphwalker.example/YouTubeTest.java
@@ -0,0 +1,88 @@
+package org.graphwalker.example;
+
+import org.graphwalker.core.annotations.Before;
+import org.graphwalker.core.annotations.GraphWalker;
+import org.graphwalker.core.utils.Assert;
+import org.graphwalker.example.base.SeleniumBasedTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Random;
+
+@GraphWalker(id = "youtubeSenario", model = "models/YouTubeModel.graphml", group = "youtube")
+public class YouTubeTest extends SeleniumBasedTest {
+
+    private final String myStartPage = System.getProperty("start.page");
+    private String myCurrentFeedTitle = null;
+    private String myCurrentVideoTitle = null;
+    private WebElement myCurrentGuideElement = null;
+    
+    @Before
+    public void assertTestPreconditions() {
+       Assert.assertNotNull(myStartPage);
+    }
+
+    public void e_startpage() {
+        getWebDriver().navigate().to(System.getProperty("start.page"));
+    }
+    
+    public void v_startpage() {
+        Assert.assertTrue(System.getProperty("start.page").equals(getWebDriver().getCurrentUrl()));
+    }
+
+    public void e_search() {
+        WebElement webElement = getWebDriver().findElement(By.id("masthead-search-term"));
+        webElement.sendKeys("model based testing");
+        webElement.submit();
+    }
+
+    public void v_search() {
+        WebElement searchHeader = getWebDriver().findElement(By.id("search-header-inner"));
+        Assert.assertNotNull(searchHeader);
+        Assert.assertTrue(searchHeader.getText().contains("model based testing"));
+    }
+
+    public void e_home() {
+        getWebDriver().findElement(By.id("logo")).click();
+    }
+
+    public void e_guide() {
+        List<WebElement> webElements = getWebDriver().findElements(By.cssSelector("a.guide-item"));
+        myCurrentGuideElement = webElements.get(new Random().nextInt(webElements.size()));
+        myCurrentGuideElement.click();
+    }
+
+    public void v_guide() {
+        Assert.assertNotNull(myCurrentGuideElement);
+        Assert.assertTrue(myCurrentGuideElement.isDisplayed());
+        Assert.assertTrue(myCurrentGuideElement.isEnabled());
+        Assert.assertTrue(myCurrentGuideElement.getAttribute("class").contains("selected"));
+    }
+
+    public void e_chooseVideo() {
+        List<WebElement> webElements = getWebDriver().findElements(By.cssSelector("a.yt-uix-tile-link"));
+        WebElement webElement = webElements.get(new Random().nextInt(webElements.size()));
+        myCurrentVideoTitle = webElement.getText();
+        webElement.click();
+    }
+
+    public void v_chooseVideo() {
+        Assert.assertNotNull(myCurrentVideoTitle);
+        WebElement titleElement = getWebDriver().findElement(By.id("eow-title"));
+        Assert.assertTrue(titleElement.getAttribute("title").equals(myCurrentVideoTitle));
+    }
+
+    public void e_chooseFeed() {
+        List<WebElement> webElements = getWebDriver().findElements(By.cssSelector("a.title.yt-uix-sessionlink"));
+        WebElement webElement = webElements.get(new Random().nextInt(webElements.size()));
+        myCurrentFeedTitle = webElement.getText();
+        webElement.click();
+    }
+
+    public void v_chooseFeed() {
+        Assert.assertNotNull(myCurrentFeedTitle);
+        WebElement titleElement = getWebDriver().findElement(By.id("eow-title"));
+        Assert.assertTrue(titleElement.getAttribute("title").equals(myCurrentFeedTitle));
+    }
+}
---
+++ graphwalker-example/src/test/java/org.graphwalker.example/base/SeleniumBasedTest.java
@@ -0,0 +1,62 @@
+package org.graphwalker.example.base;
+
+import com.thoughtworks.selenium.SeleniumException;
+import org.graphwalker.core.annotations.After;
+import org.graphwalker.core.annotations.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class SeleniumBasedTest {
+
+    private static final long DEFAULT_TIMEOUT = 30;
+    private static final BrowserType DEFAULT_BROWSER = BrowserType.FIREFOX;
+    private WebDriver myWebDriver;
+
+    @Before
+    public void createWebDriver() {
+        myWebDriver = createWebDriver(getBrowserType());
+        myWebDriver.manage().timeouts().implicitlyWait(getTimeout(), TimeUnit.SECONDS);
+    }
+
+    @After
+    public void destroyWebDriver() {
+        myWebDriver.close();
+        myWebDriver = null;
+    }
+
+    protected WebDriver getWebDriver() {
+        return myWebDriver;
+    }
+    
+    private WebDriver createWebDriver(BrowserType type) {
+        switch (type) {
+            case CHROME: return new ChromeDriver();
+            case FIREFOX: return new FirefoxDriver();
+            case IEXPLORER: return new InternetExplorerDriver();
+            default: throw new SeleniumException("Unknown browser type");
+        }
+    }
+
+    protected enum BrowserType {
+        CHROME, FIREFOX, IEXPLORER, OPERA
+    }
+
+    private BrowserType getBrowserType() {
+        if (null != System.getProperty("browser.type")) {
+            return BrowserType.valueOf(System.getProperty("browser.type"));
+        }
+        return DEFAULT_BROWSER;
+    }
+
+    private long getTimeout() {
+        if (null != System.getProperty("browser.timeout")) {
+            return Long.parseLong(System.getProperty("browser.timeout"));
+        }
+        return DEFAULT_TIMEOUT;
+    }
+    
+}
