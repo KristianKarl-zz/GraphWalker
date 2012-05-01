8bef6478ca6d6c388f95fbeb1afde1693080d2ef
Nils Olsson <nils.olsson@sylog.se>
2012-05-01 22:32:42 +0200

    added license

--- graphwalker-core/src/test/java/org/graphwalker/core/generators/RandomUnvisitedFirstPathTest.java
+++ graphwalker-core/src/test/java/org/graphwalker/core/generators/RandomUnvisitedFirstPathTest.java
@@ -1,3 +1,28 @@
+/*
+ * #%L
+ * GraphWalker Core
+ * %%
+ * Copyright (C) 2011 - 2012 GraphWalker
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
 package org.graphwalker.core.generators;
 
 import org.graphwalker.core.GraphWalker;
@@ -35,27 +60,27 @@ public class RandomUnvisitedFirstPathTest {
         Assert.assertNull(element.getName());
         element = graphWalker.getNextStep();
         Assert.assertEquals(element.getName(), "v_1");
-        Assert.assertEquals(countUnvisitedEdges((Vertex)element), 5);
+        Assert.assertEquals(countUnvisitedEdges((Vertex) element), 5);
         graphWalker.getNextStep();
         graphWalker.getNextStep();
-        Assert.assertEquals(countUnvisitedEdges((Vertex)element), 4);
+        Assert.assertEquals(countUnvisitedEdges((Vertex) element), 4);
         graphWalker.getNextStep();
         graphWalker.getNextStep();
-        Assert.assertEquals(countUnvisitedEdges((Vertex)element), 3);
+        Assert.assertEquals(countUnvisitedEdges((Vertex) element), 3);
         graphWalker.getNextStep();
         graphWalker.getNextStep();
-        Assert.assertEquals(countUnvisitedEdges((Vertex)element), 2);
+        Assert.assertEquals(countUnvisitedEdges((Vertex) element), 2);
         graphWalker.getNextStep();
         graphWalker.getNextStep();
-        Assert.assertEquals(countUnvisitedEdges((Vertex)element), 1);
+        Assert.assertEquals(countUnvisitedEdges((Vertex) element), 1);
         graphWalker.getNextStep();
-        Assert.assertEquals(countUnvisitedEdges((Vertex)element), 0);
+        Assert.assertEquals(countUnvisitedEdges((Vertex) element), 0);
         Assert.assertFalse(graphWalker.hasNextStep());
     }
 
     private int countUnvisitedEdges(Vertex vertex) {
         int count = 0;
-        for (Edge edge: vertex.getEdges()) {
+        for (Edge edge : vertex.getEdges()) {
             if (!edge.isVisited()) {
                 count++;
             }
