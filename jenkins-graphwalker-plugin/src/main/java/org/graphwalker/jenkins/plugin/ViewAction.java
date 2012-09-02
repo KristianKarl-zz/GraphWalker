66a8db355332a05356bb36359b557766dd9d5df5
Nils Olsson <nils.olsson@sylog.se>
2012-09-03 00:00:16 +0200

    Added a new ViewAction that will be used to display the result and execution time for all jobs in a view

--- jenkins-graphwalker-plugin/src/main/java/org/graphwalker/jenkins/plugin/ViewAction.java
+++ jenkins-graphwalker-plugin/src/main/java/org/graphwalker/jenkins/plugin/ViewAction.java
@@ -1,14 +1,20 @@
 package org.graphwalker.jenkins.plugin;
 
-import hudson.model.Action;
-import hudson.model.View;
+import hudson.model.*;
+import org.kohsuke.stapler.bind.JavaScriptMethod;
 
-public class ViewAction implements Action {
+import java.util.*;
+
+public class ViewAction implements RootAction, ModelObject {
 
     private final View myView;
 
     public ViewAction(View view) {
         myView = view;
+    }
+
+    public View getView() {
+        return myView;
     }
 
     public String getIconFileName() {
@@ -23,7 +29,33 @@ public class ViewAction implements Action {
         return PluginImpl.URL_NAME;
     }
 
-    public View getView() {
-        return myView;
+    @JavaScriptMethod
+    public List<String> getJobNames() {
+        List<String> names = new ArrayList<String>();
+        for (TopLevelItem item: getView().getItems()) {
+            names.add(item.getName());
+        }
+        Collections.sort(names);
+        return names;
+    }
+
+    @JavaScriptMethod
+    public Map<String,String> getJobAttributes(String name) {
+        Map<String,String> attributes = new HashMap<String,String>();
+        Job job = (Job)getView().getItem(name);
+        if (((AbstractProject)job).isDisabled()) {
+            attributes.put("isDisabled", "true");
+        } else {
+            attributes.put("isBuilding", job.isBuilding()?"true":"false");
+            if (job.isBuilding()) {
+                Executor executor = job.getLastBuild().getExecutor();
+                attributes.put("isLikelyStuck", executor.isLikelyStuck()?"true":"false");
+                attributes.put("progress", ""+executor.getProgress());
+            } else {
+                attributes.put("success", job.getLastBuild().getResult().isWorseThan(Result.SUCCESS)?"false":"true");
+                attributes.put("failure", job.getLastBuild().getResult().isBetterThan(Result.FAILURE)?"false":"true");
+            }
+        }
+        return attributes;
     }
 }
