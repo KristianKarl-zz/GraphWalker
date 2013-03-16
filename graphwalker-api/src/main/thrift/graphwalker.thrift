0566a37ae2ba1754e65d44466d85b96437df8291
Nils Olsson <nils.olsson@sylog.se>
2013-03-16 07:01:30 +0100

    Added thrift definition file

---
+++ graphwalker-api/src/main/thrift/graphwalker.thrift
@@ -0,0 +1,27 @@
+namespace c_glib GraphWalkerAPI
+namespace java org.graphwalker.api
+namespace cpp org.graphwalker.api
+namespace rb GraphWalker.API
+namespace perl GraphWalkerAPI
+namespace csharp GraphWalker.API
+namespace js GraphWalkerAPI
+namespace st GraphWalkerAPI
+namespace py GraphWalkerAPI
+namespace py.twisted GraphWalkerAPI
+namespace go GraphWalkerAPI
+namespace php GraphWalkerAPI
+namespace delphi GraphWalker.API
+namespace cocoa GraphWalkerAPI
+namespace * org.graphwalker.api
+
+service ConfigurationService {
+
+}
+
+service ExecutionService {
+
+}
+
+service ResultService {
+
+}
