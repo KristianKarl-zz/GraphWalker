0bc4fdf4ab25879987d8331a401b73258e866d4f
Nils Olsson <nils.olsson@arcticblue.se>
2013-07-05 13:41:26 +0200

    Implemented the Squarified Treemap algorithm

--- graphwalker-jenkins-plugin/src/main/webapp/scripts/script.js
+++ graphwalker-jenkins-plugin/src/main/webapp/scripts/script.js
@@ -1,27 +1,31 @@
 
 
-var elements = new Array(new Treemap.Element(21, 20)
-    , new Treemap.Element(20, 20)
-    , new Treemap.Element(19, 20)
-    , new Treemap.Element(18, 20)
-    , new Treemap.Element(17, 20)
-    , new Treemap.Element(16, 20)
-    , new Treemap.Element(15, 20)
-    , new Treemap.Element(14, 20)
-    , new Treemap.Element(13, 20)
-    , new Treemap.Element(12, 20)
-    , new Treemap.Element(11, 20)
-    , new Treemap.Element(10, 20)
-    , new Treemap.Element(9, 20)
-    , new Treemap.Element(1, 20)
-    , new Treemap.Element(1, 20)
-    , new Treemap.Element(1, 20)
-    , new Treemap.Element(1, 20)
-    , new Treemap.Element(1, 20)
-    , new Treemap.Element(1, 20)
-    , new Treemap.Element(1, 20)
-    , new Treemap.Element(1, 20)
-);
+var elements = [
+    new Treemap.Group([new Treemap.Element(21, 20)
+        , new Treemap.Element(20, 20)
+        , new Treemap.Element(19, 20)
+        , new Treemap.Element(18, 20)
+        , new Treemap.Element(1, 20)
+    ], 1),
+    new Treemap.Group([new Treemap.Element(16, 20)
+        , new Treemap.Element(15, 20)
+        , new Treemap.Element(14, 20)
+        , new Treemap.Element(13, 20)
+        , new Treemap.Element(12, 20)
+        , new Treemap.Element(1, 20)
+    ], 2),
+    new Treemap.Group([new Treemap.Element(10, 20)
+        , new Treemap.Element(9, 20)
+        , new Treemap.Element(2, 20)
+        , new Treemap.Element(3, 20)
+        , new Treemap.Element(1, 20)
+        , new Treemap.Element(1, 20)
+        , new Treemap.Element(1, 20)
+        , new Treemap.Element(1, 20)
+        , new Treemap.Element(1, 20)
+        , new Treemap.Element(1, 20)
+    ], 3)
+];
 
 function draw() {
     var context = document.getElementById('canvas').getContext('2d');
