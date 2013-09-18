ff0ec94a9e6dd949f0703e57d6f455ab64d8a79b
Nils Olsson <nils.olsson@arcticblue.se>
2013-09-18 23:23:20 +0200

    Updated API + Core

--- graphwalker-api/src/main/java/org/graphwalker/api/event/MachineSink.java
+++ graphwalker-api/src/main/java/org/graphwalker/api/event/MachineSink.java
@@ -1,7 +1,10 @@
 package org.graphwalker.api.event;
 
+import org.graphwalker.api.graph.Element;
+
 /**
  * @author Nils Olsson
  */
 public interface MachineSink extends Sink {
+    void walking(Element element);
 }
--- graphwalker-api/src/main/java/org/graphwalker/api/event/ModelSink.java
+++ graphwalker-api/src/main/java/org/graphwalker/api/event/ModelSink.java
@@ -1,7 +1,12 @@
 package org.graphwalker.api.event;
 
+import org.graphwalker.api.graph.Edge;
+import org.graphwalker.api.graph.Vertex;
+
 /**
  * @author Nils Olsson
  */
 public interface ModelSink extends Sink {
+    void edgeAdded(Edge edge);
+    void vertexAdded(Vertex vertex);
 }
