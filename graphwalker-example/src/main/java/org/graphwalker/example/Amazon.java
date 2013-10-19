52dda89db3ef43d56b8f30327f507ad07e6f5360
Nils Olsson <nils.olsson@arcticblue.se>
2013-10-19 15:17:26 +0200

    Updated changed class name and package structure

--- graphwalker-example/src/main/java/org/graphwalker/example/Amazon.java
+++ graphwalker-example/src/main/java/org/graphwalker/example/Amazon.java
@@ -1,13 +1,13 @@
 package org.graphwalker.example;
 
-import org.graphwalker.core.annotations.Execute;
-import org.graphwalker.core.annotations.GraphWalker;
+import org.graphwalker.core.annotation.Execute;
+import org.graphwalker.core.annotation.GraphWalker;
 import org.graphwalker.core.common.Assert;
-import org.graphwalker.core.conditions.support.EdgeCoverage;
-import org.graphwalker.core.conditions.support.ReachedVertex;
-import org.graphwalker.core.generator.support.AStarPath;
-import org.graphwalker.core.generator.support.RandomPath;
-import org.graphwalker.core.script.Context;
+import org.graphwalker.core.condition.EdgeCoverage;
+import org.graphwalker.core.condition.ReachedVertex;
+import org.graphwalker.core.generator.AStarPath;
+import org.graphwalker.core.generator.RandomPath;
+import org.graphwalker.core.script.ScriptContext;
 import org.graphwalker.example.models.ShoppingCart;
 import org.openqa.selenium.By;
 
@@ -29,48 +29,48 @@ import org.openqa.selenium.By;
 public class Amazon extends AbstractTest implements ShoppingCart {
 
     @Override
-    public void e_ShoppingCart(Context context) {
+    public void e_ShoppingCart(ScriptContext context) {
         getDriver().findElement(By.id("nav-cart")).click();
     }
 
     @Override
-    public void v_BrowserStarted(Context context) {
+    public void v_BrowserStarted(ScriptContext context) {
         Assert.assertNotNull(getDriver());
     }
 
     @Override
-    public void v_OtherBoughtBooks(Context context) {
+    public void v_OtherBoughtBooks(ScriptContext context) {
         Assert.assertTrue(verifyTextPresent(By.id("hlb-upsell"), "Customers Who Bought "));
     }
 
     @Override
-    public void e_EnterBaseURL(Context context) {
+    public void e_EnterBaseURL(ScriptContext context) {
         getDriver().get("http://www.amazon.com");
     }
 
     @Override
-    public void e_AddBookToCart(Context context) {
+    public void e_AddBookToCart(ScriptContext context) {
         getDriver().findElement(By.id("bb_atc_button")).click();
     }
 
     @Override
-    public void e_SearchBook(Context context) {
+    public void e_SearchBook(ScriptContext context) {
         getDriver().findElement(By.id("twotabsearchtextbox")).clear();
         getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys("Model-based testing");
         getDriver().findElement(By.id("twotabsearchtextbox")).submit();
     }
 
     @Override
-    public void e_StartBrowser(Context context) {
+    public void e_StartBrowser(ScriptContext context) {
         getDriver();
     }
 
     @Override
-    public void v_BookInformation(Context context) {
+    public void v_BookInformation(ScriptContext context) {
     }
 
     @Override
-    public void v_ShoppingCart(Context context) {
+    public void v_ShoppingCart(ScriptContext context) {
         Assert.assertTrue(verifyTitle("^Amazon\\.com Shopping Cart.*"));
         Integer expected_num_of_books = (Integer)context.getAttribute("num_of_books");
         if (expected_num_of_books.equals(0)) {
@@ -82,12 +82,12 @@ public class Amazon extends AbstractTest implements ShoppingCart {
     }
 
     @Override
-    public void v_SearchResult(Context context) {
+    public void v_SearchResult(ScriptContext context) {
         Assert.assertNotNull(getDriver().findElement(By.linkText("Practical Model-Based Testing: A Tools Approach")));
     }
 
     @Override
-    public void v_BaseURL(Context context) {
+    public void v_BaseURL(ScriptContext context) {
         Assert.assertTrue(verifyTitle("^Amazon\\.com:.*$"));
         Integer num_of_books = Integer.parseInt(getDriver().findElement(By.id("nav-cart-count")).getText());
         if (!num_of_books.equals(context.getAttribute("num_of_books"))) {
@@ -96,17 +96,17 @@ public class Amazon extends AbstractTest implements ShoppingCart {
     }
 
     @Override
-    public void e_ClickBook(Context context) {
+    public void e_ClickBook(ScriptContext context) {
         getDriver().findElement(By.linkText("Practical Model-Based Testing: A Tools Approach")).click();
     }
 
     @Override
-    public void v_ClearShoppingCart(Context context) {
+    public void v_ClearShoppingCart(ScriptContext context) {
         Assert.assertEquals(Integer.parseInt(getDriver().findElement(By.id("nav-cart-count")).getText()), 0);
     }
 
     @Override
-    public void e_ClearShoppingCart(Context context) {
+    public void e_ClearShoppingCart(ScriptContext context) {
         getDriver().findElement(By.id("nav-cart")).click();
         getDriver().findElement(By.linkText("Delete")).click();
     }
--- graphwalker-example/src/main/java/org/graphwalker/example/Google.java
+++ graphwalker-example/src/main/java/org/graphwalker/example/Google.java
@@ -1,8 +1,8 @@
 package org.graphwalker.example;
 
-import org.graphwalker.core.annotations.GraphWalker;
+import org.graphwalker.core.annotation.GraphWalker;
 import org.graphwalker.core.common.Assert;
-import org.graphwalker.core.script.Context;
+import org.graphwalker.core.script.ScriptContext;
 import org.graphwalker.example.models.google.GoogleStart;
 import org.graphwalker.example.models.google.SearchImage;
 import org.graphwalker.example.models.google.SearchText;
@@ -13,35 +13,35 @@ import org.openqa.selenium.By;
 public class Google extends AbstractTest implements GoogleStart, SearchText, SearchImage, SearchYoutube {
 
     @Override
-    public void e_SearchImage(Context context) {
+    public void e_SearchImage(ScriptContext context) {
         getDriver().findElement(By.id("gb_2")).click();
         getDriver().findElement(By.id("lst-ib")).sendKeys("model based test framework");
         getDriver().findElement(By.id("tsf")).submit();
     }
 
     @Override
-    public void v_Google(Context context) {
+    public void v_Google(ScriptContext context) {
     }
 
     @Override
-    public void v_Search(Context context) {
+    public void v_Search(ScriptContext context) {
         Assert.assertFalse(true);
     }
 
     @Override
-    public void e_VisitGoogle(Context context) {
+    public void e_VisitGoogle(ScriptContext context) {
         getDriver().get("http://www.google.se");
     }
 
     @Override
-    public void e_SearchYoutube(Context context) {
+    public void e_SearchYoutube(ScriptContext context) {
         getDriver().findElement(By.id("gb_36")).click();
         getDriver().findElement(By.id("masthead-search-term")).sendKeys("model based test framework");
         getDriver().findElement(By.id("masthead-search")).submit();
     }
 
     @Override
-    public void e_SearchText(Context context) {
+    public void e_SearchText(ScriptContext context) {
         getDriver().findElement(By.id("gbqfq")).sendKeys("model based test framework");
         getDriver().findElement(By.id("gbqf")).submit();
     }
